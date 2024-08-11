f079b5875a6054bde042fe384437ea3b972aef77
AnnR <ann_rommm@mail.ru>
2024-08-11 14:41:50 +0300

    Step 3

---
+++ src/Cli.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace  Php\Project\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function greetUser()
+{
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, {$name}!");
+    //return $name;
+}
